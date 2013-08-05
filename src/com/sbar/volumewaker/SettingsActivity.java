5a019ef8df5c972cb5dac46fe270486bf731c03e
Alexander Lopatin <sbar.geek@gmail.com>
2013-08-05 15:18:00 +0400

    don't run a service if it already runs; cleared manifest

--- src/com/sbar/volumewaker/SettingsActivity.java
+++ src/com/sbar/volumewaker/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.sbar.volumewaker;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.Intent;
@@ -10,13 +11,31 @@ import com.sbar.volumewaker.BootService;
 
 public class SettingsActivity extends Activity {
     private final String LOGTAG = "volumewaker SettingsActivity";
+
+    private boolean isServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(
+            Context.ACTIVITY_SERVICE
+        );
+        for (ActivityManager.RunningServiceInfo service :
+             manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (BootService.class.getName().equals(
+                    service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public void onCreate(Bundle s) {
         super.onCreate(s);
         setContentView(R.layout.main);
 
-        Intent mServiceIntent = new Intent(this, BootService.class);
-        startService(mServiceIntent);
+        if (!isServiceRunning())
+        {
+            Intent serviceIntent = new Intent(this, BootService.class);
+            startService(serviceIntent);
+        }
         Log.i(LOGTAG, LOGTAG+" onCreate");
     }
 }
